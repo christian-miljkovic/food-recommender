c235455681b75a41c682b29acab3c21c2b6d57a0
Christian Miljkovic <cmm892@stern.nyu.edu>
2017-07-08 17:09:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def webhook():
 
     res = json.dumps(res, indent=4)
     # print(res)
-    r = make_response(res)
+    r = make_response(jsonify(res))
     r.headers['Content-Type'] = 'application/json'
     return r
 
